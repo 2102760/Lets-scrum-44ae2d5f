02cdc5a1b7a03ef6124d67407d04ff07551856eb
BeauvR <beauvanrouwendal@gmail.com>
2019-12-11 14:23:34 +0100

    àdd icon to cahnge

--- chrome_extension/loadDarkmode.js
+++ chrome_extension/loadDarkmode.js
@@ -11,5 +11,42 @@ chrome.storage.sync.get(['darkmode'], function(items) {
                 head.appendChild(link)
             ;
         });
+        var rightSide =document.getElementsByClassName("right-side")[0],
+            switchen=document.createElement("div");
+            switchen.onclick = function() { 
+                chrome.storage.sync.set({'darkmode': false});
+                window.location.href = window.location.href;
+            };
+            switchen.style = "margin-top: auto; margin-bottom: auto; padding-right: 10px; cursor: pointer;",
+            switchen.innerHTML='<i class="material-icons">nights_stay</i>',
+            rightSide.insertBefore(switchen, rightSide.firstChild)
+        ;
+    } else {
+        var rightSide =document.getElementsByClassName("right-side")[0],
+            switchen=document.createElement("div");
+            switchen.onclick = function() { 
+                chrome.storage.sync.set({'darkmode': true});
+                window.location.href = window.location.href;
+            };
+            switchen.style = "margin-top: auto; margin-bottom: auto; padding-right: 10px; cursor: pointer;",
+            switchen.innerHTML='<i class="material-icons">wb_sunny</i>',
+            rightSide.insertBefore(switchen, rightSide.firstChild)
+        ;
     }
 });
+
+document.getElementsByTagName("app-user-credits")[0].style.borderLeft = "1px solid #ccc";
+document.getElementsByClassName("credit")[0].style.paddingLeft = "10px";
+
+function changeMode(state) {
+    if (state) {
+        alert('1');
+        chrome.storage.sync.set({'darkmode': true}, function() {
+            console.log('Value is set to ' + value);
+        });
+    } else {
+        chrome.storage.sync.set({'darkmode': false}, function() {
+            console.log('Value is set to ' + value);
+        });
+    }
+}
