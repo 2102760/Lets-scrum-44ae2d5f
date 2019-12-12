aea0717e4142ba594a9ceb1de62e37b802841ee4
Aaron Jonk <2119311@talnet.nl>
2019-12-12 12:28:26 +0100

    Dablit

---
+++ aaron_stuff/popup.js
@@ -0,0 +1,24 @@
+const toggle = document.querySelector('.toggle-input');
+const initialState = localStorage.getItem('toggleState') == 'true';
+toggle.checked = initialState;
+
+toggle.addEventListener('change', function () {
+  if (toggle.checked) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        { code: 'var head=document.getElementsByTagName("HEAD")[0],link=document.createElement("link");link.rel="stylesheet",link.id="dark",link.type="text/css",link.href="' + chrome.runtime.getURL('dark.css') + '",head.appendChild(link);' 
+      });
+    });
+  } else {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        {
+          code: 'document.getElementById("dark").remove();'
+        });
+    });
+  }
+
+  localStorage.setItem('toggleState', toggle.checked);
+});
