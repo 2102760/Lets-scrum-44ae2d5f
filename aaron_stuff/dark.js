485538ee2eb040729e1cd079ece38cf06c9ff2b1
BeauvR <beauvanrouwendal@gmail.com>
2019-12-20 12:20:36 +0100

    removed some files

--- aaron_stuff/dark.js
+++
@@ -1,7 +0,0 @@
-// hello.mjs
-export function hello(text) {
-    console.log('test21');
-    const div = document.createElement('div');
-    div.textContent = `Hello ${text}`;
-    document.body.appendChild(div);
-}
